ba10f698521b615b9f996c5f9ae526971d941f0d
Nataliia Hirniak <gn.natalia19@gmail.com>
2026-05-01 21:15:53 +0100

    feat: add speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import App from "./App.tsx";
 
@@ -10,6 +11,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Analytics />
+    <SpeedInsights />
     <App />
   </StrictMode>,
 );
